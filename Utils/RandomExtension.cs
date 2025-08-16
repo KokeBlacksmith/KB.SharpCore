220f87623587f8eea7e328313041ddc068718ab3
KokeBlacksmith <47671664+KokeBlacksmith@users.noreply.github.com>
2025-08-16 17:13:37 +0200

    random extensions for floats

---
+++ Utils/RandomExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace KB.SharpCore.Utils;
+
+/// <summary>
+/// Extension methods for <see cref="Random"/> to provide additional functionality.
+/// </summary>
+public static class RandomExtension
+{
+    public static float NextSingle(this Random rng, float minValue, float maxValue)
+    {
+        if (minValue >= maxValue)
+        {
+            throw new ArgumentOutOfRangeException(nameof(minValue), "minValue must be less than maxValue.");
+        }
+
+        return (float)(rng.NextDouble() * (maxValue - minValue) + minValue);
+    }
+
+    public static float NextSingle(this Random rng, float maxValue)
+    {
+        if (maxValue <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(maxValue), "maxValue must be greater than 0.");
+        }
+
+        return (float)(rng.NextDouble() * maxValue);
+    }
+}
